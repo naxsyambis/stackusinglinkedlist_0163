e300189744baa6ba4907524e59a5727383e60d76
naxsyambis <naxsyambis@gmail.com>
2024-05-11 12:02:03 +0100

    menampilkan pilihan perintah

--- stackusinglinkedlist_0163/stackusinglinkedlist_0163.cpp
+++ stackusinglinkedlist_0163/stackusinglinkedlist_0163.cpp
@@ -86,6 +86,13 @@ int main()
 
 	while (choice != 5)
 	{
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Peek\n";
+		cout << "4. Exit\n";
+		cout << "Enter your choice: \n";
+		cin >> choice;
+
 
 	}
 
