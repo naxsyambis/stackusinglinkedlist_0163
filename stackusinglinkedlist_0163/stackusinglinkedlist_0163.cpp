bf2a2cec9f27e2539a54468a3dca5f4b2caeea9e
naxsyambis <naxsyambis@gmail.com>
2024-05-11 11:55:15 +0100

    return the value of the top node

--- stackusinglinkedlist_0163/stackusinglinkedlist_0163.cpp
+++ stackusinglinkedlist_0163/stackusinglinkedlist_0163.cpp
@@ -56,8 +56,19 @@ public:
 		{
 			cout << "List is empty." << endl;
 		}
+		else {
+			Node* current = top;
+			while (current != NULL)
+			{
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}// return the value of the top node
 
 	}
+
+
 
 
 };
