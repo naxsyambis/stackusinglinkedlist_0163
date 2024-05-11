b299976cef2cdd1e68dc99f8d1f8ae4b8fef6778
naxsyambis <naxsyambis@gmail.com>
2024-05-11 12:03:04 +0100

    case 1: memasukkan value dan push ke stack

--- stackusinglinkedlist_0163/stackusinglinkedlist_0163.cpp
+++ stackusinglinkedlist_0163/stackusinglinkedlist_0163.cpp
@@ -93,6 +93,14 @@ int main()
 		cout << "Enter your choice: \n";
 		cin >> choice;
 
+		switch (choice)
+		{
+		case 1:
+			cout << "Enter the value to push: ";
+			cin >> value;
+			stack.push(value); // Push the entered value onto the stack
+			break;
+
 
 	}
 
