b2b869a168ef2f4f109c293d510d9a65f7fcb8c0
naxsyambis <naxsyambis@gmail.com>
2024-05-11 11:54:13 +0100

    jika top == NULL tampilkan list empty

--- stackusinglinkedlist_0163/stackusinglinkedlist_0163.cpp
+++ stackusinglinkedlist_0163/stackusinglinkedlist_0163.cpp
@@ -52,6 +52,10 @@ public:
 	// peek/top operation: retrieve the value of the topmost element without removing it
 	void peek()
 	{
+		if (top == NULL)
+		{
+			cout << "List is empty." << endl;
+		}
 
 	}
 
